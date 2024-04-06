229ee6fbe4433cd6b835e37325896f4aa33012af
vishuofficial <vk6672304@gmail.com>
2024-04-07 00:45:39 +0530

    Complete

--- Music Player/Js/music.js
+++ Music Player/Js/music.js
@@ -1,77 +1,63 @@
 let progress = document.querySelector("#progress");
 let song = document.querySelector("#song");
 let ctrlIcon = document.querySelector("#ctrlIcon");
-let backwardbtn = document.querySelector("#bcwrd");
-let forwardbtn = document.querySelector("#frward");
 
-
-let songPlaylist = [{ 
-                     songName : "Suno Na Sangmarmar",
-                     src : "../audio/Suno Na Sangemarmar (SlowedReverb)  Arijit Singh  Zamina.mp3",
-                     artistName : "Unknown(pagalworld.mp3)",
-                     img : "../Images/image1.jpg",
-                    },
-
-                    {songName : "Waalian",
-                     src : "../audio/Waalian - Harnoor.mp3",
-                     artistName : "Harnoor(pagalworld.mp3)",
-                     img : "../Images/image1.jpg",
-                    },
-
-                    {songName : "Naah",
-                     src : "../audio/Naah(PagalWorld.com.se).mp3",
-                     artistName : "Jass Manak",
-                     img : "../Images/image1.jpg",
-                    },
-
-                    {songName : "Parshawan",
-                     src :  "../audio/Parshawan - Harnoor.mp3",
-                     artistName : "Harnoor(pagalworld.mp3)",
-                     img : "../Images/image1.jpg",
-                    },
+let songPlaylist = [
+{id : 1,
+ songName : "Suno Na Sangmarmar",
+ src : "../audio/Suno Na Sangemarmar (SlowedReverb)  Arijit Singh  Zamina.mp3",
+ artistName : "Unknown(pagalworld.mp3)",
+ img : "../Images/image1.jpg",
+},
+{id : 2,
+ songName : "Waalian",
+ src : "../audio/Waalian - Harnoor.mp3",
+ artistName : "Harnoor(pagalworld.mp3)",
+ img : "../Images/Waaliyan.jpg",
+},
+{id : 3,
+ songName : "Naah",
+ src : "../audio/Naah(PagalWorld.com.se).mp3",
+ artistName : "Jass Manak",
+ img : "../Images/naah.jpg",
+},
+{id : 4,
+ songName : "Parshawan",
+ src :  "../audio/Parshawan - Harnoor.mp3",
+ artistName : "Harnoor(pagalworld.mp3)",
+ img : "../Images/parshawan.jpg",
+},
 ];
 
 
-let songName = document.querySelector(".songName");
-let artistName = document.querySelector(".artist-name");
-let img = document.querySelector("img");
-let nextSongBtn = document.querySelector(".NextSong");
+let fbtnClickCount = 1;
+let forwardBtn = document.querySelector("#forward");
 
+forwardBtn.addEventListener("click", ()=>{
+    let index = fbtnClickCount;
+    source = document.querySelector("source").src = songPlaylist[index].src;
+    songName = document.querySelector(".songName").innerText = songPlaylist[index].songName;
+    artistName = document.querySelector(".artist-name").innerText = songPlaylist[index].artistName;
+    thumbImage = document.querySelector("#image").src = songPlaylist[index].img;
+    song.load();
+    autoPlaySong();
+    fbtnClickCount++;
+} );
 
+let bbtnClickCount = 3;
 
-// function NextSong(){
-//     // for (const song of songPlaylist) {
-//     //     let currentIdx = 0;
-//     //     songPlaylist = songPlaylist[cur];
-//     //     source = document.querySelector("#source").src = currentIdx.src;
-//     //     song.load();
-//     //     autoPlaySong();
-//     // } currentIdx++;
-//     // console.log(currentIdx);
-//     if(currentIndex<songPlaylist.length-1){
-//         currentIndex++;
-//         displaydata();
-//     } else{
-//         console.log("No more song");
-//     }
-// }
+let backwardBtn = document.querySelector("#backward");
 
-function setSongSrc(){
-    source = document.querySelector("#source");
-    source.src = songPlaylist[currentIndex].src;
-}
-
-setSongSrc();
-
-forwardbtn.addEventListener("click", () =>{
-    currentIndex = (currentIndex-1+songPlaylist.length)% songPlaylist.length;
-    setSongSrc();
-    song.play();
-} )
-
-function displaydata(){
-    const currentData = songPlaylist[currentIndex];
-}
+backwardBtn.addEventListener("click", ()=>{
+    let index = bbtnClickCount;
+    source = document.querySelector("source").src = songPlaylist[index].src;
+    songName = document.querySelector(".songName").innerText = songPlaylist[index].songName;
+    artistName = document.querySelector(".artist-name").innerText = songPlaylist[index].artistName;
+    thumbImage = document.querySelector("#image").src = songPlaylist[index].img;
+    song.load();
+    autoPlaySong();
+    bbtnClickCount--;
+} );
 
 song.onloadedmetadata = function(){
     progress.max = song.duration;
